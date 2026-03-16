029e8723c9548d5726c74461355103578fa7a606
ANDREY AVDEENKO <avdey.1996@mail.ru>
2026-03-16 20:22:09 +0300

    корректирока 5 задание

--- dz2.sql
+++ dz2.sql
@@ -1,15 +1,14 @@
 --1.Название и продолжительность самого длительного трека.
-SELECT name, duration FROM db.tracks WHERE duration = (SELECT MAX(duration) FROM db.tracks)
+SELECT name, duration FROM tracks WHERE duration = (SELECT MAX(duration) FROM tracks);
 
 --2.Название треков, продолжительность которых не менее 3,5 минут.
-SELECT name FROM db.tracks WHERE duration / 60 >= 3.5
+SELECT name FROM tracks WHERE duration / 60 >= 3.5;
 
 --3.Названия сборников, вышедших в период с 2018 по 2020 год включительно
-SELECT name FROM db.collection WHERE release_year BETWEEN 2018 AND 2020
+SELECT name FROM collection WHERE release_year BETWEEN 2018 AND 2020;
 
 --4.Исполнители, чьё имя состоит из одного слова
-SELECT name FROM db.artists WHERE name NOT LIKE '% %';
+SELECT name FROM artists WHERE name NOT LIKE '% %';
 
 --5.Название треков, которые содержат слово «мой» или «my».
-SELECT name FROM db.tracks WHERE name ILIKE '%мой%' OR LOWER(name) ILIKE '%my%';
-
+SELECT name FROM tracks WHERE string_to_array(LOWER(name), ' ') && ARRAY['мой', 'my'];
